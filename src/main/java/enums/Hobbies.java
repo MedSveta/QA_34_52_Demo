ecb89cb1cec0b93e58118601e027e7e0d8251d5a
MedSveta <sveta1978medved@gmail.com>
2026-09-15 12:05:20 +0300

    SoftAssert

--- src/main/java/enums/Hobbies.java
+++ src/main/java/enums/Hobbies.java
@@ -1,9 +1,9 @@
 package enums;
 
 public enum Hobbies {
-    SPORTS("//*[@]for='hobbies-checkbox-1']"),
-    READING("//*[@]for='hobbies-checkbox-2']"),
-    MUSIC("//*[@]for='hobbies-checkbox-3']");
+    SPORTS("//*[@for='hobbies-checkbox-1']"),
+    READING("//*[@for='hobbies-checkbox-2']"),
+    MUSIC("//*[@for='hobbies-checkbox-3']");
 
     private String locator;
 
